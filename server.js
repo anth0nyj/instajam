5514a232661f42444d1e089b7c1211392c87587f
Anthony <ajtalvarez@gmail.com>
2018-01-22 19:56:07 -0500

    Basic IM built.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+  console.log("An artist has joined!");
+  socket.on('disconnect', () => {
+    console.log('An artist has disconnected.');
+    io.emit("chat message", "An artist has disconnected.");
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.broadcast.emit('hi');
+});
+
+const PORT = 3001;
+http.listen(PORT, () => {
+  console.log("InstaJam Running on Port: ", PORT);
+});
