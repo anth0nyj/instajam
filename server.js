ce1ce127ee3ae9ecb9b4e1c89affbe5f9fba74b5
Anthony <ajtalvarez@gmail.com>
2018-01-26 22:09:05 -0500

    Refactored server.js. Refactored io for chat. Added io for audio.

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const PORT = 3001;
+const socket = require('socket.io');
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 const Octavian = require('octavian');
+
+const server = app.listen(PORT, () => {
+  console.log('======================================');
+  console.log("   InstaJam Running on Port: ", PORT);
+  console.log('======================================');
+});
+
+const io = socket(server);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 io.on('connection', (socket) => {
-  console.log("An artist has joined!");
-  socket.on('disconnect', () => {
-    console.log('An artist has disconnected.');
-    io.emit("chat message", "An artist has disconnected.");
+  console.log("Socket Connected: ", socket.id);
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+
+  socket.on('sound-pad', (play) => {
+    io.sockets.emit('sound-pad', play)
   });
-  socket.broadcast.emit('hi');
 });
 
-const PORT = 3001;
-http.listen(PORT, () => {
-  console.log("InstaJam Running on Port: ", PORT);
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('Socket Connected: ', socket.id);
+//   socket.on('disconnect', () => {
+//     console.log('An artist has disconnected.');
+//     io.emit("chat message", "An artist has disconnected.");
+//   });
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//   });
+//   socket.broadcast.emit('hi');
+// });
