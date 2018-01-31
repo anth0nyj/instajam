0e56f7dd4b869a0c0b06bdc3bc7500e9da8e1074
Anthony <ajtalvarez@gmail.com>
2018-01-31 09:29:31 -0500

    Fixed port for Heroku.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const socket = require('socket.io');
 // const http = require('http').Server(app);
 // const io = require('socket.io')(http);
