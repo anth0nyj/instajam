ccd3eb46b61dd4581a9699ff7515de12bc90395d
Anthony <ajtalvarez@gmail.com>
2018-01-31 09:39:58 -0500

    Resolving cross origin issues.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const socket = require('socket.io');
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
 const Octavian = require('octavian');
 
 const server = app.listen(PORT, () => {
@@ -27,19 +25,3 @@ io.on('connection', (socket) => {
     io.sockets.emit('sound-pad', play)
   });
 });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('Socket Connected: ', socket.id);
-//   socket.on('disconnect', () => {
-//     console.log('An artist has disconnected.');
-//     io.emit("chat message", "An artist has disconnected.");
-//   });
-//   socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg);
-//   });
-//   socket.broadcast.emit('hi');
-// });
